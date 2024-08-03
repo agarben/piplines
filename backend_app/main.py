d6d0475e8d914e5cb0dbb603724241c9679d8b9c
Ben Agar <agarben@gmail.com>
2024-08-03 15:07:02 +0300

    fix formatting

--- backend_app/main.py
+++ backend_app/main.py
@@ -9,24 +9,22 @@ app = FastAPI()
 # Configure CORS
 orig_origins = [
     "http://localhost",  # Allow this origin
-    "http://localhost:80",  # Allow this origin (if your frontend is served from port 3000)
+    "http://localhost:80",  # Allow this origin (frontend at port 80)
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=orig_origins,  # Allows all origins (or specify only necessary origins)
+    allow_origins=orig_origins,
     allow_credentials=True,
-    allow_methods=["*"],  # Allows all methods (GET, POST, etc.)
-    allow_headers=["*"],  # Allows all headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
 @app.get("/data")
 def read_data():
-    x = 5
-    return {
-        "message": f"Hello from backend container: {os.environ.get('HOSTNAME', 'DEFAULT_ENV')}"
-    }
+    hostname = os.environ.get("HOSTNAME", "DEFAULT_ENV")
+    return {"message": f"Hello from backend container: {hostname}"}
 
 
 def add(a: int, b: int) -> int:
